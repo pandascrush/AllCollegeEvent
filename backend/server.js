78bc1a767a61e3ab0f25292deb683a2272fb51da
Siva V <velayuthamsiva55@gmail.com>
2025-11-14 10:16:13 +0530

    feat: setup commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend server running successfully");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running at http://localhost:5000");
+});
